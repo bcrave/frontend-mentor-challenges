ca7fa75438537b7e6ad0dc28182e16cfe19fbf26
Brendon Crave <brennycode@gmail.com>
2023-06-22 17:57:06 +0400

    routing started

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-import "./App.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import Home from "./components/Home";
+import About from "./components/About";
+
+import RootLayout from "./layouts/RootLayout";
+import SolutionsLayout from "./layouts/SolutionsLayout";
+
+import "./styles/App.scss";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="solutions" element={<SolutionsLayout />}></Route>
+    </Route>
+  )
+);
 
 function App() {
-  return <div className="app">Your Component Here</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
