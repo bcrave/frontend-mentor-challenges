e2372d5e8f63c356d2cee4f9258edac8b43a002a
Brendon Crave <brennycode@gmail.com>
2023-06-25 17:54:40 +0400

    removed unused import

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { NavLink, Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 type RootStyles = {
   backgroundColor: string;
